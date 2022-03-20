13d6ae92a631f39be465d7147d56695e40c4fe50
gabaaa <not-set>
2022-03-19 20:41:17 -0500

    first commit after clone

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: semver
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.semver import semver
+# from .gpkgs import message as msg
+# from .gpkgs.nargs import Nargs
+# from .gpkgs.etconf import Etconf
